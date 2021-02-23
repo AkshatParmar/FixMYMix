9e3f235f85fcaf97a71343fc496c7fe1d6729886
Akshat Parmar <akshatparmar@Akshats-MacBook-Air.local>
2021-02-22 21:23:44 -0500

    Added Spotipy to requirements

--- api/main.py
+++ api/main.py
@@ -4,5 +4,11 @@ from flask_restful import Api, Resource, abort, reqparse
 app = Flask(__name__)
 api = Api(app)
 
+class SpotipyMethods(Resource):
+	def get(self):
+		return {"data":"Hello World"}
+
+api.add_resource(SpotipyMethods, "/init")
+
 if __name__ == "__main__":
 	app.run(debug=True)
