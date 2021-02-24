c4f43d2fd4f72ffaffca01c7b3a633114f8bd0b0
Akshat Parmar <akshatparmar@Akshats-MacBook-Air.local>
2021-02-24 16:41:54 -0500

    Initial API

--- api/main.py
+++ api/main.py
@@ -1,14 +1,30 @@
 from flask import Flask, request 
 from flask_restful import Api, Resource, abort, reqparse
+import os
+import spotipy
+from spotipy import oauth2
+import logger
 
+# Environment variables read from server side
+myClientID = os.environ.get('client_id')
+mySecret = os.environ.get('secret')
+
+## Req {redirectURI, scope, cache_sys}
 app = Flask(__name__)
 api = Api(app)
 
 class SpotipyMethods(Resource):
-	def get(self):
-		return {"data":"Hello World"}
+	def get_access_token(self, name, user):
+		access_token = ''
+		return {"name":name, "user":user}
 
-api.add_resource(SpotipyMethods, "/init")
+	def get_playlists(self, user_id):
+		return
+
+	def user_profile(self, user_id):
+		return
+
+api.add_resource(SpotipyMethods, "/init/<string:name>/<string:user>")
 
 if __name__ == "__main__":
 	app.run(debug=True)
