7d7d5b6514b332064f5920beeb5d7f35f9e93424
Akshat Parmar <akshatparmar@akshats-air.myfiosgateway.com>
2021-03-22 23:01:13 -0400

    Updated README, called flask endpoint from React

--- project/api/main.py
+++ project/api/main.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource, abort, reqparse
 import os
 import spotipy
 from spotipy import oauth2
+import time
 
 # Environment variables read from server side
 myClientID = os.environ.get('client_id')
@@ -12,16 +13,21 @@ mySecret = os.environ.get('secret')
 app = Flask(__name__)
 api = Api(app)
 
+''' 	Test method     '''
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
 class SpotipyMethods(Resource):
 	def get_access_token(self, name, user):
 		access_token = ''
 		return {"name":name, "user":user}
 
 	def get_playlists(self, user_id):
-		return
+		return {"Playlist User":user_id}
 
 	def user_profile(self, user_id):
-		return
+		return {"User name": user_id}
 
 api.add_resource(SpotipyMethods, "/init/<string:name>/<string:user>")
 
--- project/src/components/App.js
+++ project/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from 'react'
 import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
@@ -11,27 +11,44 @@ import UpdateProfile from "./UpdateProfile"
 
 
 function App() {
-     
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
-    <Container
-      className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "1200px" }}>
-        <Router>
-          <AuthProvider>
-            <Switch>
-              <PrivateRoute exact path="/" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-            </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
-  )
+    <div className="App">
+      <header className="App-header">
+
+
+        <p>The current time is {currentTime}.</p>
+      </header>
+    </div>
+  );
+     
+  // return (
+  //   <Container
+  //     className="d-flex align-items-center justify-content-center"
+  //     style={{ minHeight: "100vh" }}
+  //   >
+  //     <div className="w-100" style={{ maxWidth: "1200px" }}>
+  //       <Router>
+  //         <AuthProvider>
+  //           <Switch>
+  //             <PrivateRoute exact path="/" component={Dashboard} />
+  //             <PrivateRoute path="/update-profile" component={UpdateProfile} />
+  //             <Route path="/signup" component={Signup} />
+  //             <Route path="/login" component={Login} />
+  //             <Route path="/forgot-password" component={ForgotPassword} />
+  //           </Switch>
+  //         </AuthProvider>
+  //       </Router>
+  //     </div>
+  //   </Container>
+  // )
 }
 
 export default App
