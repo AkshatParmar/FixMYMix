a428cb4536e98213ab2c8954c9d6a33012fe10df
Gabe Ajram <gabrielajram@gmail.com>
2021-03-29 13:50:23 -0400

    Buttons added

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react" ;
 import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
@@ -8,16 +8,26 @@ import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Themes" ;
 
 
 function App() {
-     
+  const [theme, setTheme] = useState('light')
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light') }  
+    //const icon= props.theme === "light" ? <HiMoon size={40} /> : CgSun size={40}/>;
   return (
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}> 
+      <>
+      <GlobalStyles/>
+      <button onClick={themeToggler}>Switch Theme</button>
     <Container
       className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
+      style={{ minHeight: "100vh"}}
     >
-      <div className="w-100" style={{ maxWidth: "1200px" }}>
+      <div className="w-100" style={{ maxWidth: "800px" }}>
         <Router>
           <AuthProvider>
             <Switch>
@@ -31,6 +41,8 @@ function App() {
         </Router>
       </div>
     </Container>
+    </>
+    </ThemeProvider>//added
   )
 }
 
