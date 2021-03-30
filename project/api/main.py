e680251c1dcc23d7bea330aad08d1d784c0f802f
Akshat Parmar <akshatparmar@akshats-air.myfiosgateway.com>
2021-03-30 14:11:29 -0400

    Last commit before pull request

--- project/api/main.py
+++ project/api/main.py
@@ -3,11 +3,15 @@ from flask_restful import Api, Resource, abort, reqparse
 import os
 import spotipy
 from spotipy import oauth2
+import spotipy.util as util
 import time
 
 # Environment variables read from server side
 myClientID = os.environ.get('REACT_APP_SPOTIFY_CLIENT_ID')
-mySecret = os.environ.get('secret')
+mySecret = os.environ.get('REACT_APP_SPOTIFY_CLIENT_SECRET')
+myRedirect = os.environ.get('REACT_APP_SPOTIFY_REDIRECT_URI')
+myUsername = '22njnu2ho3sr42yphgpf53zqq'
+scope = 'user-library-read'
 
 ## Req {redirectURI, scope, cache_sys}
 app = Flask(__name__)
@@ -21,7 +25,43 @@ def get_current_time():
 
 @app.route('/info')
 def get_info():
-	return {'client_id':myClientID}
+	return {'client_id':myClientID,
+			'client secret':mySecret,
+			'redirect uri':myRedirect,
+			'User ID':myUsername,
+			'Scope':scope}
+
+@app.route('/login')
+def login():
+	token = util.prompt_for_user_token(myUsername, scope, myClientID, mySecret, myRedirect)
+	if token:
+		return token 
+	else:
+		return 'Please sign in'
+
+''' Helper Function  '''
+def show_tracks(tracks):
+	for i, item in enumerate(tracks['items']):
+		track = item['track']
+		return ("   %d %32.32s %s" % (i, track['artists'][0]['name'],track['name']))
+
+@app.route('/playlists')
+def get_playlists():
+	token = util.prompt_for_user_token(myUsername, scope, myClientID, mySecret, myRedirect)
+	sp = spotipy.Spotify(auth=token)
+	playlists = sp.user_playlists(myUsername)
+	for playlist in playlists['items']:
+		if playlist['owner']['id'] == myUsername:
+			print(playlist['name'])
+			print ('  total tracks', playlist['tracks']['total'])
+			results = sp.playlist(playlist['id'],fields="tracks,next")
+			tracks = results['tracks']
+			while tracks['next']:
+				tracks = sp.next(tracks)
+				output = show_tracks(tracks)
+	return playlists
+
+
 
 class SpotipyMethods(Resource):
 	def get_access_token(self, name, user):
