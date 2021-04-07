aa30245caafa7225231bfef1f7609334ac348229
Akshat Parmar <akshatparmar@akshats-air.myfiosgateway.com>
2021-04-06 21:45:02 -0400

    Deleted all other directories, project/ updated

--- project/src/components/App.js
+++ project/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react" ;
 import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
@@ -8,55 +8,42 @@ import Login from "./Login"
 import PrivateRoute from "./PrivateRoute"
 import ForgotPassword from "./ForgotPassword"
 import UpdateProfile from "./UpdateProfile"
+import {ThemeProvider} from "styled-components";
+import { GlobalStyles } from "./GlobalStyles";
+import { lightTheme, darkTheme } from "./Themes" ;
 
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [clientID, setClientID] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.date);
-    });
-  }, []);
-
-  useEffect(() => {
-    fetch('/info').then(res => res.json()).then(data => {
-      setClientID(data.client_id);
-    });
-  }, []);
-
+  const [theme, setTheme] = useState('light')
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light') }  
+    //const icon= props.theme === "light" ? <HiMoon size={40} /> : CgSun size={40}/>;
   return (
-    <div className="App">
-      <header className="App-header">
-
-
-        <p>The current date time is {currentTime}.</p>
-        <p>The client ID is {clientID}</p>
-      </header>
-    </div>
-  );
-     
-  // return (
-  //   <Container
-  //     className="d-flex align-items-center justify-content-center"
-  //     style={{ minHeight: "100vh" }}
-  //   >
-  //     <div className="w-100" style={{ maxWidth: "1200px" }}>
-  //       <Router>
-  //         <AuthProvider>
-  //           <Switch>
-  //             <PrivateRoute exact path="/" component={Dashboard} />
-  //             <PrivateRoute path="/update-profile" component={UpdateProfile} />
-  //             <Route path="/signup" component={Signup} />
-  //             <Route path="/login" component={Login} />
-  //             <Route path="/forgot-password" component={ForgotPassword} />
-  //           </Switch>
-  //         </AuthProvider>
-  //       </Router>
-  //     </div>
-  //   </Container>
-  // )
+    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}> 
+      <>
+      <GlobalStyles/>
+      <button onClick={themeToggler}>Switch Theme</button>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh"}}
+    >
+      <div className="w-100" style={{ maxWidth: "800px" }}>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
+    </>
+    </ThemeProvider>//added
+  )
 }
 
 export default App
